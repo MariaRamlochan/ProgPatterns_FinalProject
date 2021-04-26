c07ae0c35ff0124fe0ea65447e72424212ec17b4
MariaRamlochan <mariaramlochan@hotmail.com>
2021-04-26 10:52:29 -0400

    Added the create tables in the controller

--- src/finalproject/FlightController.java
+++ src/finalproject/FlightController.java
@@ -1,5 +1,6 @@
 package finalproject;
 
+import java.sql.*;
 import java.util.*;
 
 /**
@@ -9,4 +10,40 @@ import java.util.*;
 public class FlightController {
     private List<Flight> model;
     private FlightView view;
+    
+    private final Connection conn = FlightDBConnection.getInstance();
+    
+    public void createFlightsTable(String sqlStatement) {
+        try(Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate("DROP TABLE IF EXIST Flights;");
+            stmt.executeUpdate(sqlStatement); 
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+    
+    public void createClientsTable(String sqlStatement) {
+        try(Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate("DROP TABLE IF EXIST Clients;");
+            stmt.executeUpdate(sqlStatement); 
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+    
+    public void createReservedFlightsTable(String sqlStatement) {
+        try(Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate("DROP TABLE IF EXIST ReservedFlights;");
+            stmt.executeUpdate(sqlStatement); 
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+    
+    public void updateView(Map model) {
+        FlightView.printFlightDetails(model);
+    }
 }
