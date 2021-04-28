4fb0e566d2362a71da9a25e89b81deb68384297b
MariaRamlochan <mariaramlochan@hotmail.com>
2021-04-28 19:02:56 -0400

    client table

--- src/finalproject/Client.java
+++ src/finalproject/Client.java
@@ -1,5 +1,6 @@
 package finalproject;
 
+import java.sql.*;
 import java.util.*;
 
 /**
@@ -10,8 +11,23 @@ public class Client {
     private String fullName; //full name of the client
     private int passNumber;
     private int contact;    //contact number of the client
+     private final Connection clientConn = ClientDBConnection.getInstance();
     
     public boolean bookASeat(String fn){ 
+        Flight flight;
+        try (Statement stmt = clientConn.createStatement()) {
+            //if (flight.available = true) {
+                String sql = "INSERT INTO Clients (passNumber, fullName, contact) " 
+                    + "VALUES (" + passNumber + ", '" + fullName + "',' " 
+                    + contact + "');";
+                System.out.println("sql: " + sql);
+                stmt.execute(sql);
+                return true;
+            //} 
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
         return false;
     }
     
