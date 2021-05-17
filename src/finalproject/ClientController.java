e4db17982114f6812827174a2c7fbce0d18f84c2
MariaRamlochan <mariaramlochan@hotmail.com>
2021-05-16 22:04:15 -0400

    JavaDocs for ClientController class

--- src/finalproject/ClientController.java
+++ src/finalproject/ClientController.java
@@ -4,19 +4,31 @@ import java.sql.*;
 import java.util.*;
 
 /**
- *
- * @author maria
+ * Class to control the client through a controller
+ * @author maria and nafees
  */
 public class ClientController {
+    //Data members
     private List<Client> model;
     private ClientView view;
+    
+    //Client connection
     private final Connection clientConn = ClientDBConnection.getInstance();
 
+    /**
+     * COnstructor with all parameters
+     * @param model the list of clients
+     * @param view the view to view the clients
+     */
     public ClientController(List<Client> model, ClientView view) {
         this.model = model;
         this.view = view;
     }
  
+    /**
+     * Create a table of clients
+     * @param sqlStatement the sql statement
+     */
     public void createClientsTable(String sqlStatement) {
         try (Statement stmt = clientConn.createStatement()) {
             stmt.executeUpdate("DROP TABLE IF EXISTS CLIENTS;");
@@ -27,6 +39,11 @@ public class ClientController {
         }
     }
     
+    /**
+     * Method to update the view of the client
+     * @param model a map of the client
+     * @return a string of the printed clients from the map
+     */
     public String updateView(Map model) {
         return ClientView.printClientDetails(model);
     }
