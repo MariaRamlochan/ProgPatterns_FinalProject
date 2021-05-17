8f10e3a0435f902616d5d20dfd410325c082a453
MariaRamlochan <mariaramlochan@hotmail.com>
2021-05-16 22:10:42 -0400

    JavaDocs for ClientView class

--- src/finalproject/ClientView.java
+++ src/finalproject/ClientView.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package finalproject;
 
 import java.util.Map;
 
 /**
- *
- * @author Natsu
+ * Class to print the details from a client map
+ * @author maria and nafees
  */
 public class ClientView {
     public static String printClientDetails(Map map) {
